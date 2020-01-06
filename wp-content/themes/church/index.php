4a62b7d174e1fb408f7965f8e69eea29cd8dcfc3
Bishoy Romany <7bishoyromany7@gmail.com>
2020-01-07 01:23:33 +0200

    Finished Pages

--- wp-content/themes/church/index.php
+++ wp-content/themes/church/index.php
@@ -40,7 +40,9 @@
                  * get the main cats posts max 5
                  */
                 $pages = me_pages_menu();
+                $x = 0;
                 foreach($pages as $page){
+                    $x ++;
                     $id = get_post_meta($page->ID , '_menu_item_object_id', true);
                     $page = get_post($id);
                     /**
@@ -53,23 +55,49 @@
                     else: 
                         $image = false;
                     endif;
+                    if($x < 4):
                     ?>
-                    <div class="col-md-3 pages-container-home">
-                        <div class="content text-center">
-                            <!-- image container -->
-                            <a class="img-container" href="<?php echo $url; ?>">
-                                <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>">
-                            </a>
-                            <h3> <a href="<?php echo $url; ?>"> <?php echo $title; ?></a></h3>
-                            <?php if(strlen($page->post_content)): ?>
-                                <p><?php echo $page->post_content; ?></p>
-                            <?php endif; ?>
+                        <div class="col-md-3 pages-container-home">
+                            <div class="content text-center">
+                                <!-- image container -->
+                                <a class="img-container" href="<?php echo $url; ?>">
+                                    <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>">
+                                </a>
+                                <h3> <a href="<?php echo $url; ?>"> <?php echo $title; ?></a></h3>
+                                <?php if(strlen($page->post_content)): ?>
+                                    <p><?php echo $page->post_content; ?></p>
+                                <?php endif; ?>
+                            </div>
                         </div>
-                    </div>
+                    <?php else: 
+                        if($x == 4){
+                            echo "<div class='clear'></div>";
+                        }    
+                        
+                    ?>
+                        <div class="pages-container-home-flex move-left" style="
+                        <?php if($x == 4){echo 'margin-left:5px;';} ?> ">
+                            <div class="content text-center">
+                                <!-- image container -->
+                                <a class="img-container" href="<?php echo $url; ?>">
+                                    <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>">
+                                </a>
+                                <h3> <a href="<?php echo $url; ?>"> <?php echo $title; ?></a></h3>
+                                <?php if(strlen($page->post_content)): ?>
+                                    <p><?php echo $page->post_content; ?></p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
                     <?php
+                    endif;
                 }
 
             ?>
+
+            <!-- up comding events -->
+            <div class="upcoming-events pull-right">
+                Comming Events
+            </div>
 
 
             <!-- right sidebar -->
